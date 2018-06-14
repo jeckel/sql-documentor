6bcbc485f5cd576ed21efa1874fd1499437aefa4
Jeckel <jeckel@jeckel.fr>
2018-06-14 19:22:37 +0200

    Update configuration

--- config/sql-documentor.config.php
+++ config/sql-documentor.config.php
@@ -9,16 +9,18 @@ return [
         'password' => getenv('MYSQL_PASSWORD')?:'',
     ],
     'path' => [
-        'templates' => getenv('TEMPLATE_DIRECTORY')?:(PROJECT_ROOT.'tpl/'),
-        'target'    => getenv('TARGET_DIRECTORY')?:(PROJECT_ROOT.'docs/'),
-        'yml'       => getenv('YML_DIRECTORY')?:(PROJECT_ROOT.'yml/'),
+        'template' => getenv('TEMPLATE_DIRECTORY')?:(PROJECT_ROOT.'tpl/'),
+        'target'   => getenv('TARGET_DIRECTORY')?:(PROJECT_ROOT.'docs/'),
+        'yml'      => getenv('YML_DIRECTORY')?:(PROJECT_ROOT.'yml/'),
     ],
     'service_manager' => [
         'factories' => [
             'dbh'                                                                      => \SqlDocumentor\Factory\DbConnectionFactory::class,
             'logger'                                                                   => \SqlDocumentor\Factory\LoggerFactory::class,
             \SqlDocumentor\Services\DbParser::class                                    => \SqlDocumentor\Services\DbParserFactory::class,
+            \SqlDocumentor\Services\SqlDocumentor::class                               => \SqlDocumentor\Services\SqlDocumentorFactory::class,
             \SqlDocumentor\Services\TableBuilder::class                                => \SqlDocumentor\Services\TableBuilderFactory::class,
+            \SqlDocumentor\Services\Generator\FileGenerator::class                     => \SqlDocumentor\Services\Generator\FileGeneratorFactory::class,
             \SqlDocumentor\Services\Hydrator\YamlHydrator::class                       => \SqlDocumentor\Services\Hydrator\YamlHydratorFactory::class,
             \SqlDocumentor\Services\Hydrator\YamlHydrator\FileParser::class            => \SqlDocumentor\Services\Hydrator\YamlHydrator\FileParserFactory::class,
             \SqlDocumentor\Services\Hydrator\SQLHydrator::class                        => \SqlDocumentor\Services\Hydrator\SQLHydratorFactory::class,
@@ -27,10 +29,10 @@ return [
         'invokables' => [
             \SqlDocumentor\Model\ColumnFactory::class                                  => \SqlDocumentor\Model\ColumnFactory::class,
             \SqlDocumentor\Model\TableFactory::class                                   => \SqlDocumentor\Model\TableFactory::class,
-            \SqlDocumentor\Services\TemplateProcessor::class => \SqlDocumentor\Services\TemplateProcessor::class,
             \SqlDocumentor\Services\Hydrator\SQLHydrator\TableHydrator::class          => \SqlDocumentor\Services\Hydrator\SQLHydrator\TableHydrator::class,
             \SqlDocumentor\Services\Hydrator\YamlHydrator\ColumnHydrator::class        => \SqlDocumentor\Services\Hydrator\YamlHydrator\ColumnHydrator::class,
-            \SqlDocumentor\Services\Hydrator\YamlHydrator\TableHydrator::class         => \SqlDocumentor\Services\Hydrator\YamlHydrator\TableHydrator::class
+            \SqlDocumentor\Services\Hydrator\YamlHydrator\TableHydrator::class         => \SqlDocumentor\Services\Hydrator\YamlHydrator\TableHydrator::class,
+            \SqlDocumentor\Services\Generator\TemplateParser::class                    => \SqlDocumentor\Services\Generator\TemplateParser::class,
         ],
         'initializers' => [
             \SqlDocumentor\Initializer\LoggerInitializer::class,
